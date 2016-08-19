2c26c71213b11e97c099d94e0c70423ca9bc27cf
brettkoko <brett@kokodigital.co.uk>
2016-08-19 12:40:30 +0100

    Update CDVInAppBrowser.m
    added custom url scheme

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -403,7 +403,7 @@
         }
     } 
     //if is an app store link, let the system handle it, otherwise it fails to load it
-    else if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"]) {
+    else if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"]  || [[ url scheme] isEqualToString:@"nrsa"] || [[ url scheme] isEqualToString:@"nrrb"]) {
         [theWebView stopLoading];
         [self openInSystem:url];
         return NO;
